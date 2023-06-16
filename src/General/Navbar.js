5fe017777225176391e367a6597cabda8946cf02
elodie-saintaubin <backup.esa00@gmail.com>
2023-06-16 14:08:21 +0200

    Navbar responsive

--- src/General/Navbar.js
+++ src/General/Navbar.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
-// Import the PNG icon file
 import icon from '../Planets/meteore.png';
 
 function Navbar() {
@@ -12,38 +11,74 @@ function Navbar() {
     setIsActive(!isActive);
   };
 
-  const closeMenu = () => {
-    setIsActive(false);
-  };
   return (
-    <div>
-      <nav className="navbar">
-        <div className="navbar-icon">
-          <img src={icon} alt="Icon" />
-          <h1>Meteo(re)</h1>
+    <div className='toutNavbar'>
+      
+    <nav className={`navbar ${isActive ? 'active' : ''}`}>
+      <div className={`navbar-icon ${isActive ? 'to-align' : ''}`}>
+        <img src={icon} alt="Icon" />
+        <h1>Meteo(re)</h1>
+      </div>
+      <div className={`hamburger ${isActive ? 'active to-align' : ''}`} onClick={toggleMenu}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
+        <div className="navbar-none">
+          <ul className="nav-menu">
+            <li className="nav-link">
+              <Link to="/la-meteore" onClick={toggleMenu}>
+                Accueil
+              </Link>
+            </li>
+            <li className="nav-link">
+              <Link to="/planets" onClick={toggleMenu}>
+                Planètes
+              </Link>
+            </li>
+            <li className="nav-link">
+              <Link to="/apod" onClick={toggleMenu}>
+                Picture of the day
+              </Link>
+            </li>
+            <li className="nav-link">
+              <Link to="/game" onClick={toggleMenu}>
+                A little game ?
+              </Link>
+            </li>
+          </ul>
         </div>
-  
-        <div className="hamburger" onClick={toggleMenu}>
-          <span className={`bar ${isActive ? 'active' : ''}`}></span>
-          <span className={`bar ${isActive ? 'active' : ''}`}></span>
-          <span className={`bar ${isActive ? 'active' : ''}`}></span>
+
+    </nav>
+    {isActive && (
+        <div className="mobile-menu">
+          <ul className="nav-menu">
+            <li className="nav-link">
+              <Link style={{color:"white", textDecoration:"none"}} to="/la-meteore" onClick={toggleMenu}>
+                Accueil
+              </Link>
+            </li>
+            <li className="nav-link">
+              <Link style={{color:"white", textDecoration:"none"}} to="/planets" onClick={toggleMenu}>
+                Planètes
+              </Link>
+            </li>
+            <li className="nav-link">
+              <Link style={{color:"white", textDecoration:"none"}} to="/apod" onClick={toggleMenu}>
+                Picture of the day
+              </Link>
+            </li>
+            <li className="nav-link">
+              <Link style={{color:"white", textDecoration:"none"}} to="/game" onClick={toggleMenu}>
+                A little game ?
+              </Link>
+            </li>
+          </ul>
         </div>
-        <ul className={`nav-menu ${isActive ? 'active' : ''}`}>
-          <li className="nav-link" onClick={closeMenu}>
-            <Link to="/la-meteore">Accueil</Link>
-          </li>
-          <li className="nav-link" onClick={closeMenu}>
-            <Link to="/planets">Planètes</Link>
-          </li>
-          <li className="nav-link" onClick={closeMenu}>
-            <Link to="/apod">Picture of the day</Link>
-          </li>
-          <li className="nav-link" onClick={closeMenu}>
-            <Link to="/game">A little game ?</Link>
-          </li>
-        </ul>
-      </nav>
+      )}
+    
     </div>
+    
   );
 }
 
